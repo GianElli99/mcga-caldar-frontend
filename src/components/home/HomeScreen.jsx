54900a50f5dfbfdbc31e3c04a9bb3f85dde6dca8
Gianfranco Elli <gianelli99@hotmail.com>
2021-11-12 19:01:05 -0300

    basic modal test added in home screen

--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { NONE } from '../../redux/types/modalTypes';
 import { GenericModal } from '../shared/GenericModal';
 
 export const HomeScreen = () => {
-  return (
-    <div>
-      <GenericModal></GenericModal>
-    </div>
-  );
+  const { actionInProgress } = useSelector((state) => state.technicians);
+  return <div>{actionInProgress === NONE && <GenericModal />}</div>;
 };
